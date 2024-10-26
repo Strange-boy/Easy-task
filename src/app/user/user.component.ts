94ef896b04900b50152395aa18b8ca7abfd88c6d
Strange-boy <joel.panvel@gmail.com>
2024-10-26 15:37:16 +0530

    Learning string interpolation in angular (modifying users) 💻

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+//in order to find the random index of the dummy array
+const randomIdx = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -6,4 +10,6 @@ import { Component } from '@angular/core';
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
-export class UserComponent {}
+export class UserComponent {
+  selectedUser = DUMMY_USERS[randomIdx];
+}
