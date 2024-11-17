a0f58b1ca6bb9e7f0dcad6e59703c0fbef8af11b
Strange-boy <joel.panvel@gmail.com>
2024-11-17 16:53:02 +0530

    working with signals 📡

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 //in order to find the random index of the dummy array
@@ -11,15 +11,12 @@ const randomIdx = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIdx];
-
-  get imagePath() {
-    return 'assets/users/' + this.selectedUser.avatar;
-  }
+  selectedUser = signal(DUMMY_USERS[randomIdx]);
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
 
   //in order to change the users on click
   onSelectUser() {
     const randomIdx = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser = DUMMY_USERS[randomIdx];
+    this.selectedUser.set(DUMMY_USERS[randomIdx]);
   }
 }
